ec31f27d35bca6141d53f93773698a0d8c5aa723
Seth Wright <sethwright@WTMBPSethWright.lan>
2023-12-02 08:38:15 -0500

    Day 1 part 2.

--- aoc2023/day01.py
+++ aoc2023/day01.py
@@ -1,32 +1,53 @@
+from typing import Tuple
+import re
+
 LIMIT = 1000
 data: list[str] = open("aoc2023/inputs/day01.txt", "r").read().splitlines()
 
+word_map = {
+    "one": 1,
+    "twone": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+for i in range(1, 10):
+    word_map[str(i)] = i
 
-def find_first_last_digit(line: str) -> (int, int):
-    start_chars = line
-    end_chars = start_chars[::-1]
-    start_digit = extract_first_int(start_chars)
-    end_digit = extract_first_int(end_chars)
+r_forward = r"(\d|one|two|three|four|five|six|seven|eight|nine)"
+r_backward = r"(\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)"
+
+
+def find_first_last_digit(line: str) -> Tuple[int, int]:
+    # reverse the line
+    line_bw = line[::-1]
+
+    match_forward = re.search(r_forward, line)
+    match_backward = re.search(r_backward, line_bw)
+
+    if not match_forward or not match_backward:
+        raise ValueError("No match found for: " + line)
+
+    first = match_forward.group(0)
+    last = match_backward.group(0)
+    last = last[::-1]
+
+    if not first or not last:
+        raise ValueError("Missing matching group" + line)
+
+    start_digit = word_map.get(first)
+    end_digit = word_map.get(last)
+
+    if not start_digit or not end_digit:
+        raise ValueError("Missing map value" + first + " " + last)
+
     print(line, start_digit, end_digit)
     return (start_digit, end_digit)
-
-
-def extract_first_int(chars: str) -> int:
-    n = 0
-    while n < LIMIT:
-        start_n = get_int_from_str(chars[n])
-        n += 1
-        if start_n is not False:
-            return start_n
-
-
-def get_int_from_str(char: str) -> bool | int:
-    try:
-        n = int(char)
-    except:
-        return False
-
-    return n
 
 
 def main(data: list[str]):
